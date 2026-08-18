9a853d4aa1593610024759c1e78220a735ec5546
Clacki <skthk6613@naver.com>
2026-08-18 11:50:07 +0900

    feat: 장소 카드 컴포넌트 추가 (#57)

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,17 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {/* config options here */};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/photo-1548013146-72479768bada",
+        search: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
